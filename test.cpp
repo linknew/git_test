f41ba4b65f4a26570d2d5beacb451b470c1e70e3
linknew <linknew@sina.com>
2016-03-21 23:42:44 +0800

    add note1, about default construction

--- test.cpp
+++ test.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+namespace test1_1 {
+    void func(int &)
+    {
+        cout << __PRETTY_FUNCTION__ << endl ;
+        return ;
+    }
+
+    void func(const int &)
+    {
+        cout << __PRETTY_FUNCTION__ << endl ;
+        return ;
+    }
+}
+
+namespace test1_2 {
+    void func(const int &)
+    {
+        cout << __PRETTY_FUNCTION__ << endl ;
+        return ;
+    }
+}
+
+namespace test1_3 {
+    void func(int &)
+    {
+        cout << __PRETTY_FUNCTION__ << endl ;
+        return ;
+    }
+}
+
 int main(void)
 {
+    // test1 
+    {
+        int i = 3 ;
+        const int j = 4 ;
+        test1_1::func(j) ;  // ok
+        test1_1::func(i) ;  // ok
+        test1_2::func(j) ;  // ok
+        test1_2::func(i) ;  // ok
+        //test1_3::func(j) ;  // fail
+        test1_3::func(i) ;  // ok
+
+    }
     return 0 ;
 }
