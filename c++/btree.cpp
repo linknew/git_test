80f498fe28eb284d81309a8b37c10936d8e056e6
min.lee <min.lee@tpv-tech.com>
2016-03-29 19:53:32 +0800

    updat btree.cpp

--- c++/btree.cpp
+++ c++/btree.cpp
@@ -3,7 +3,6 @@
 using std::cout ;
 using std::endl ;
 
-#if 1
 template<class T>
 struct node{
     inline node(T t) : data(t),parent(NULL),left(NULL),right(NULL){} 
@@ -20,108 +19,40 @@ class btree{
         btree(): _root(NULL), _first(NULL), _last(NULL) { }
         virtual ~btree() ;
 
-        bool is_left_child(node<T>* t) const
-        {
-            return (t 
-                    && t->parent 
-                    && t->parent->left 
-                    && t->parent->left == t) ;
-        }
+#define ERR_INV_ARG  (-1)
+#define ERR_ADOPTION_FAIL   (-2)
 
-        bool is_right_child(node<T>* t) const
-        {
-            return (t 
-                    && t->parent 
-                    && t->parent->right 
-                    && t->parent->right == t) ;
-        }
-
-        bool is_root(node<T>* t) const
-        {
-            return  (t && t == _root) ;
-        }
-
-        bool has_child(node<T>* t) const
-        {
-            return  (t && (t->left || t->right)) ;
-        }
-
-        bool has_parent(node<T>* t) const
-        {
-            return  (t && (t->parent)) ;
-        }
-
-        bool has_left_child(node<T>* t) const
-        {
-            return  (t && t->left) ;
-        }
-
-        bool has_right_child(node<T>* t) const
-        {
-            return  (t && t->right) ;
-        }
-
-        bool has_left_brother(node<T>* t) const
-        {
-            return  (t  
-                     && t->parent 
-                     && t->parent->left) ;
-        }
-
-        bool has_right_brother(node<T>* t) const
-        {
-            return (t 
-                    && t->parent 
-                    && t->parent->right) ;
-        }
-
-        node<T> * get_root(void) const
-        {
-            return  _root ;
-        }
-
-        node<T> * get_left_child(node<T> *t) const
-        {
-            return  (t ? t->left : NULL) ;
-        }
-
-        node<T> * get_right_child(node<T> *t) const
-        {
-            return  (t ? t->right : NULL) ;
-        }
-
-        node<T> * get_parent(node<T> *t) const
-        {
-            return  (t ? t->parent : NULL) ;
-        }
-
+        inline bool is_left_child(node<T>* t) const ;
+        inline bool is_right_child(node<T>* t) const ;
+        inline bool is_root(node<T>* t) const ;
+        inline bool has_child(node<T>* t) const ;
+        inline bool has_parent(node<T>* t) const ;
+        inline bool has_left_child(node<T>* t) const ;
+        inline bool has_right_child(node<T>* t) const ;
+        inline bool has_brother(node<T>* t) const ;
+        inline bool has_left_brother(node<T>* t) const ;
+        inline bool has_right_brother(node<T>* t) const ;
+        inline node<T> * get_root(void) const ;
+        inline node<T> * get_parent(node<T> *t) const ;
+        inline node<T> * get_left_child(node<T> *t) const ;
+        inline node<T> * get_right_child(node<T> *t) const ;
+        inline node<T> * get_left_brother(node<T> *t) const ;
+        inline node<T> * get_right_brother(node<T> *t) const ;
+        inline node<T> * get_rightmost(node<T> *t) ;
+        inline node<T> * get_leftmost(node<T> *t) ;
         enum childid{ IAMROOT, ASLEFT, ASRIGHT } ;
-        enum order{ PREORDER, INORDER, POSTORDER } ;
+        enum traversal_type{ PREORDER, INORDER, POSTORDER } ;
 
-        inline node<T>* get_rightmost(node<T> *t)
-        { 
-            while(has_right_child(t))
-                t = get_right_child(t) ;
-            return t ;
-        }
-
-        inline node<T>* get_leftmost(node<T> *t)
-        {
-            while(has_left_child(t)) 
-                t = get_left_child(t) ;
-            return t ;
-        }
-
-        bool has_relationship(node<T> *parent, node<T> *child) ;
-        node<T>* lost_child(node<T> *parent, childid) ; //return the pool wanderer
+        node<T>* lost_child(node<T> *parent, childid id) ; //return the pool wanderer
         node<T>* leave_parent(node<T> *child) ; // return the worried parents
-        void adopt_child(node<T> *parent, node<T> *child, childid) ;
-        void traversal(node<T>* tree_top, order);
+        void adopt_child(node<T> *parent, node<T> *child, childid id) ;
+        void traversal(node<T>* tree_top, traversal_type tt = INORDER, void (*pf)(const node<T>*) = print) ;
         virtual void remove(node<T>* t) ;
-        void insertroot(node<T> *root) ;
-        void print(node<T> *t) const ;
+        static void print(const node<T> *t) ;
         childid get_childid(node<T> *child) ;
-        node<T>* get_next(node<T> *cur, order) ;
+        node<T>* get_first(node<T> *tree_top, traversal_type tt) ;
+        node<T>* get_last(node<T> *tree_top, traversal_type tt) ;
+        node<T>* get_next(node<T> *cur, traversal_type tt) ;
 
     private:
         node<T> *_root ;
@@ -130,45 +61,159 @@ class btree{
 } ;
 
 template<class T>
-void btree<T>::traversal(node<T> *t, order ot)
+inline bool btree<T>::is_left_child(node<T>* t) const
+{
+    return (t 
+            && t->parent 
+            && t->parent->left 
+            && t->parent->left == t) ;
+}
+
+template<class T>
+inline bool btree<T>::is_right_child(node<T>* t) const
+{
+    return (t 
+            && t->parent 
+            && t->parent->right 
+            && t->parent->right == t) ;
+}
+
+template<class T>
+inline bool btree<T>::is_root(node<T>* t) const
+{
+    return  (t && t == _root) ;
+}
+
+template<class T>
+inline bool btree<T>::has_child(node<T>* t) const
+{
+    return  (t && (t->left || t->right)) ;
+}
+
+template<class T>
+inline bool btree<T>::has_parent(node<T>* t) const
+{
+    return  (t && (t->parent)) ;
+}
+
+template<class T>
+inline bool btree<T>::has_left_child(node<T>* t) const
+{
+    return  (t && t->left) ;
+}
+
+template<class T>
+inline bool btree<T>::has_right_child(node<T>* t) const
+{
+    return  (t && t->right) ;
+}
+
+template<class T>
+inline bool btree<T>::has_brother(node<T>* t) const
+{
+    return  (t  
+             && t->parent 
+             && t->parent->left
+             && t->parent->right) ;
+}
+
+template<class T>
+inline bool btree<T>::has_left_brother(node<T>* t) const
+{
+    return  (t  
+             && t->parent 
+             && t->parent->left) ;
+}
+
+template<class T>
+inline bool btree<T>::has_right_brother(node<T>* t) const
+{
+    return (t 
+            && t->parent 
+            && t->parent->right) ;
+}
+
+template<class T>
+inline node<T>* btree<T>::get_rightmost(node<T> *t)
+{ 
+    while(has_right_child(t))
+        t = get_right_child(t) ;
+    return t ;
+}
+
+template<class T>
+inline node<T>* btree<T>::get_leftmost(node<T> *t)
+{
+    while(has_left_child(t)) 
+        t = get_left_child(t) ;
+    return t ;
+}
+
+template<class T>
+inline node<T> * btree<T>::get_root(void) const
+{
+    return  _root ;
+}
+
+template<class T>
+inline node<T> * btree<T>::get_left_child(node<T> *t) const
+{
+    return  (t ? t->left : NULL) ;
+}
+
+template<class T>
+inline node<T> * btree<T>::get_right_child(node<T> *t) const
+{
+    return  (t ? t->right : NULL) ;
+}
+
+template<class T>
+inline node<T> * btree<T>::get_parent(node<T> *t) const
+{
+    return  (t ? t->parent : NULL) ;
+}
+
+template<class T>
+void btree<T>::traversal(node<T> *tree_top, traversal_type tt, void (*pf)(const node<T>*))
 {
     node<T> *iterator = NULL ;
+    node<T> *start = NULL ;
+    node<T> *end = NULL ;
 
-    if(!t){
+    if(!tree_top){
         cout << "It is a empty tree" << endl ;
         return ;
     }
 
-    PREORDER == ot ? iterator = t
-                   : iterator = get_leftmost(t) ;
+    start = get_first(tree_top,tt) ;
+    end = get_last(tree_top,tt) ;
+    iterator = start ;
 
-    while(iterator != get_parent(t)){
-        print(iterator);
-        iterator = get_next(iterator,ot) ;
+    while(iterator != end){
+        if(pf)
+            pf(iterator);
+        iterator = get_next(iterator,tt) ;
     }
 
+    if(pf)
+        pf(end) ;
     return ;
 }
 
 template<class T>
 btree<T>::~btree()
 {
-    node<T> *b = NULL ;
-    node<T> *t = NULL ;
+    node<T> *d = NULL ;
+    node<T> *n = NULL ;
 
-    // free allocate memory
-    b = get_leftmost(_root) ;
+    d = get_first(_root,POSTORDER) ;
+    n = get_next(d,POSTORDER) ;
 
-    while(b)
-    {
-        t = b ; print(t) ;
-        b = get_next(b,POSTORDER) ;
-        leave_parent(t) ;
-
-        delete t ;
+    while(d){
+        remove(d) ;
+        d = n ;
+        n = get_next(n,POSTORDER) ;
     }
-
-    // no need reset members
 
     return ;
 }
@@ -176,15 +221,26 @@ btree<T>::~btree()
 template<class T>
 void btree<T>::adopt_child( node<T> *parent, node<T> *child, childid id)
 {
-    if(!parent || !child || child->parent == child) 
-        throw -1 ;
+    if(!child || (id != ASLEFT && id != ASRIGHT && id != IAMROOT))
+        throw ERR_INV_ARG ;
 
+    if(child->parent)
+        throw ERR_ADOPTION_FAIL ;
+
+    if(!_root && !parent && IAMROOT == id){
+        child->left = NULL ;
+        child->right = NULL ;
+        child->parent = NULL ;
+        _root = child ;
+        return ;
+    }
+        
     if(ASLEFT == id && parent->left == NULL)
         parent->left = child ;
     else if(ASRIGHT == id && parent->right == NULL)
         parent->right = child ;
     else
-        throw -2 ;
+        throw ERR_ADOPTION_FAIL ;
 
     child->parent = parent ;
 
@@ -192,22 +248,62 @@ void btree<T>::adopt_child( node<T> *parent, node<T> *child, childid id)
 }
 
 template<class T>
-bool btree<T>::has_relationship(node<T> *parent, node<T> *child)
+node<T>* btree<T>::get_first(node<T>* tree_top, traversal_type tt)
 {
-    if(!parent || !child)
-        throw -3 ;
+    node<T> *ret = tree_top ;
+    node<T> *t = NULL ;
 
-    return ((child->parent == parent) && 
-            (parent->left == child || parent->right == child)) ;
+    switch(tt)
+    {
+        case PREORDER:
+            ret = tree_top ;
+            break ;
+        case INORDER:
+            ret = get_leftmost(tree_top) ;
+            break ;
+        case POSTORDER:
+            while((t = get_left_child(ret)) || (t = get_right_child(ret)))
+                ret = t ;
+            break ;
+        default:
+            throw ERR_INV_ARG ;
+    }
+
+    return ret ;
 }
 
 template<class T>
-node<T>* btree<T>::get_next(node<T>* cur, btree<T>::order ot)
+node<T>* btree<T>::get_last(node<T> *tree_top, traversal_type tt)
+{
+    node<T> *ret = tree_top ;
+    node<T> *t = NULL ;
+
+    switch(tt)
+    {
+        case PREORDER:
+            while((t = get_right_child(ret)) || (t = get_left_child(ret)))
+                ret = t ;
+            break ;
+        case INORDER:
+            ret = get_rightmost(tree_top) ;
+            break ;
+        case POSTORDER:
+            ret = tree_top ;
+            break ;
+        default:
+            throw ERR_INV_ARG ;
+    }
+
+    return ret ;
+}
+
+template<class T>
+node<T>* btree<T>::get_next(node<T>* cur, traversal_type tt)
 {
     if(!cur)
         return NULL ;
 
-    switch(ot){
+    switch(tt){
         case PREORDER:
             if(has_left_child(cur))
                 return cur->left ;
@@ -313,17 +409,7 @@ node<T>* btree<T>::lost_child(node<T> *parent, childid id)
 }
 
 template<class T>
-void btree<T>::insertroot(node<T> *root)
-{
-    if(_root || !root)
-        throw -6 ;
-
-    _root = root ;
-    return ;
-}
-
-template<class T>
-void btree<T>::print(node<T>* t) const
+void btree<T>::print(const node<T>* t)
 {
     if(!t){
         cout << "Do you want to print a 'NULL' node !!?" << endl ;
@@ -333,8 +419,8 @@ void btree<T>::print(node<T>* t) const
     cout << "--data=" << t->data << endl ;
 
     if(t->parent){
-        is_left_child(t) ? cout << "  left of " << t->parent->data << endl
-                   : cout << "  right of " << t->parent->data << endl ;
+        (t->parent->left && t->parent->left == t) ? cout << "  left of " << t->parent->data << endl
+                                                  : cout << "  right of " << t->parent->data << endl ;
     }else{
         cout << "  parent = NULL" << endl ;
     }
@@ -364,7 +450,8 @@ void btree<T>::remove(node<T> *cur)
 
     // cur node has no child
     if(!has_child(cur)){
-        leave_parent(cur) ;
+        if(NULL == leave_parent(cur))   // no parent
+            _root = NULL ;
         delete cur ;
         return ;
     }
@@ -388,29 +475,43 @@ void btree<T>::remove(node<T> *cur)
 
     return ;
 }
-#endif
+
+void func(const node<int>* p)
+{
+    cout << p->data << "," ;
+    return ;
+}
 
 int main(void)
 {
+    using ::btree ;
+
     try{
         node<int> *p = NULL ;
         node<int> *q = NULL ;
 
         btree<int>  bt ;
         p = new node<int>(7) ;
-        bt.insertroot(p) ;
+        bt.adopt_child(NULL,p,btree<int>::IAMROOT) ;
         q = new node<int>(8) ;
         bt.adopt_child(p,q,btree<int>::ASRIGHT) ;
         q = new node<int>(6) ;
         bt.adopt_child(p,q,btree<int>::ASLEFT) ;
 
-        cout << "PREORDER" << endl ;
-        bt.traversal(bt.get_root(),btree<int>::PREORDER) ;
-        cout << "INORDER" << endl ;
-        bt.traversal(bt.get_root(),btree<int>::INORDER) ;
-        cout << "POSTORDER" << endl ;
-        bt.traversal(bt.get_root(),btree<int>::POSTORDER) ;
+        cout << "\n**PREORDER" << endl ;
+        bt.traversal(bt.get_root(),btree<int>::PREORDER,func) ;
 
+        cout << "\n**INORDER" << endl ;
+        bt.traversal(bt.get_root(),btree<int>::INORDER,func) ;
+
+        cout << "\n**POSTORDER" << endl ;
+        bt.traversal(bt.get_root(),btree<int>::POSTORDER,func) ;
+
+        bt.remove(bt.get_root()) ;
+        cout << "\n---------------" << endl ;
+        bt.traversal(bt.get_root(),btree<int>::POSTORDER,func) ;
+        cout << endl << "==============="  << endl ;
+        
     }catch(int &err){
         cout << err << endl ;
     }
